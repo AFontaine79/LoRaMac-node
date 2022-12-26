9847b0efa8f38d1b30060cb328427de1da111ccb
Aaron Fontaine <AFontaine79@yahoo.com>
2022-12-26 13:06:48 -0600

    Added execution tracer library to compile into ping-pong application

---
+++ src/lib/execution_tracer/src/execution_tracer.c
@@ -0,0 +1,13 @@
+/*
+ * exeuction_tracer.c
+ *
+ *  Created on: Oct 23, 2022
+ *      Author: Aaron Fontaine
+ */
+
+#include "execution_tracer.h"
+#include "execution_tracer_private.h"
+
+_Static_assert(IS_POWER_OF_2(BUFFER_LENGTH_IN_WORDS), "BUFFER_LENGTH_IN_WORDS must be a power of 2");
+
+__NO_INIT ExecTracer_t m_exec_trace;
